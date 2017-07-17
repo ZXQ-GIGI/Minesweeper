2024ac83fe38092192912b54676f6f7ac936e560
gigi <snailzhao1995@163.com>
2017-07-17 22:32:47 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,56 +1,95 @@
 'use strict'
 
-function Initial(){
-	/*var mycanvas = document.getElementById("myCanvas");
-	var bricks = new Array();
-	var bricksContext = new Array();
-	const width = 25;
-	const height = 16;
-	const mineNum = 99;
+var startMark = false;
+const mineNum = 99;
+var mines = new Array();
+var bricks = new Array();
+//
+function SetButtons(){
 
-	//var mines =  RandomMines();
-	for(var i = 0; i < width; i++){
+	startMark = true;
+	var myTable = document.getElementById("tab");
+	var rows = 0, cols = 0;
+	for(var i = 0; rows = myTable.rows.length, i < rows; i++){
+		for(var j = 0; cols = myTable.rows[i].cells.length, j < cols; j++){
 
-		bricks[i] = new Array();
-		bricksContext[i] = new Array();
-
-		for(var j = 0; j < height; j++){
-			bricksContext[i][j] = mycanvas.getContext("2d");
-			bricksContext[i][j].fillStyle = "#0ea";
-	    	bricksContext[i][j].fillRect(40 * i + 1,40 * j + 1,38,38);
-			bricks[i][j] = new Brick(bricksContext[i][j],false,-1,-1); 	
+			var id = i*cols +j;
+			var str = "<button id=" + id + " onclick = 'ClickAction(this)'>1</button>";
+			
+			if(myTable.rows[i].cells[j].className != "brick virtual"){
+				myTable.rows[i].cells[j].innerHTML = str;
+				document.getElementById(id).style.width = "30px";
+				document.getElementById(id).style.height = "30px";
+			}
 		}
 	}
-	RandomMines(bricksContext);*/
-	document.writeln("1");
+	for(var i=0; i<cols;i++){
+		bricks[i] = new Array(); 
+		for(var j=0;j<rows;j++){
+			bricks[i][j] = new Brick();
+			document.write(bricks[i][j].isMine);
+		}
+	}
+	
 }
 
+function ClickAction(obj){
+	//document.write(obj.id);
+	var id = obj.id;
+	
+	if(startMark){
+		ClickFirst(id);
+		startMark = false;
+	}
+	else{
+		document.write(mines);
+	}
+}
+
+//first time to click and set all mines
+function ClickFirst(id){
+
+	var speicialBricks = NineBricks(id);	
+					
+	//put mines randomly
+	for(var i = 0; i < mineNum; i++){
+		var randomMine = Math.floor(Math.random() * 27 * 18);
+		if(mines.indexOf(randomMine)<0 && speicialBricks.indexOf(randomMine)<0){
+			if(randomMine > 27 && randomMine < 458 && randomMine % 27 != 0 && randomMine % 27 != 26){
+				mines[i] = randomMine;	
+				document.getElementById(randomMine).style.backgroundColor = "#f00";
+			}else{
+				i--;
+			}		
+		}else{
+			i--;
+		}
+	}
+	for(var i=0; i< mines.length; i++){
+		var x = Math.floor(mines[i]/27);
+		var y = mines[i]%27;
+		bricks[x][y].SetMineState(true);
+	}	
+}
+
+function NineBricks(id){
+	return [id-1-27,id-27, id-27+1,id-1,id,id-(-1),id-1-(-27),id-(-27),id-(-27-1)];
+}
 
 // class of single brick
-function Brick(brick,isMine,totalNum,leftNum){
+function Brick(){
 
-	this.brick = brick; 		//2d
-	this.isMine = isMine; 		//bool
-	this.totalNum = totalNum; 	//total number of mines
-	this.leftNum = leftNum; 	//the number of unknown mines
+	this.isMine = false; 		//bool
+	this.totalNum = -1; 	//total number of mines
+	this.leftNum = -1; 	//the number of unknown mines
+	this.SetMineState = function(state){
+		this.isMine = state;
+	}
 }
-
-function RandomMines(bricksContext){
-	var mines = new Array();
-	for(var i = 0; i < mineNum; i++){
-		var randomMine = Math.floor(Math.random() * width * height);
-		if(mines.indexOf(randomMine)<0){
-			mines[i] = randomMine;
-			bricksContext[mines[i]%width][Math.floor(mines[i]/width)] = mycanvas.getContext("2d");
-			bricksContext[mines[i]%width][Math.floor(mines[i]/width)].fillStyle = "#ea0";
-			bricksContext[mines[i]%width][Math.floor(mines[i]/width)].fillRect(40 * i + 1,40 * j + 1,38,38);
-		}	
-		else{
-			i--;
-		}	
-	} 
-	//return mines;
-}
+/*
+Brick.prototype.SetMineState = function(state) {
+	this.isMine = state;
+};*/
 
 
 
