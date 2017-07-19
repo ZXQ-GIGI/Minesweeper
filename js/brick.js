79fd5ed66bef1166f67b3d946efd55c0909b430a
gigi <snailzhao1995@163.com>
2017-07-19 19:57:37 +0800

    finish game design

--- js/brick.js
+++ js/brick.js
@@ -1,11 +1,5 @@
 'use strict'
 
-module.exports = Brick;
+//module.exports = Brick;
 
-function Brick(brick,isMine,totalNum,leftNum){
-
-	this.brick = brick; 		//2d
-	this.isMine = isMine; 		//bool
-	this.totalNum = totalNum; 	//total number of mines
-	this.leftNum = leftNum; 	//the number of unknown mines
-}
+// class of single brick
--- js/index.js
+++ js/index.js
@@ -2,19 +2,33 @@
 
 var startMark = false;
 const mineNum = 99;
+const rows = 18;
+const cols = 27;
 var mines = new Array();
-var bricks = new Array();
-//
+var bricks = new Array(); //
+
+function Brick(){
+
+	this.isMine = false; 		//bool
+	this.totalNum = 0; 			//total number of mines
+	this.leftNum = 0; 			//the number of unknown mines
+	this.isClicked = false;
+}
+
+function PlayerBrick(){
+	this.isMine = false;
+	this.leftNum = 0;
+	this.isClicked = false;
+}
 function SetButtons(){
 
 	startMark = true;
 	var myTable = document.getElementById("tab");
-	var rows = 0, cols = 0;
 	
-	for(var i = 0; rows = myTable.rows.length, i < rows; i++){
-		for(var j = 0; cols = myTable.rows[i].cells.length, j < cols; j++){
+	for(var i = 0; i < rows; i++){
+		for(var j = 0; j < cols; j++){
 
-			var id = i*cols +j;
+			var id = i*cols + j;
 			var str = "<button id=" + id + " onclick = 'ClickAction(this)'></button>";
 			
 			if(myTable.rows[i].cells[j].className != "brick virtual"){
@@ -26,9 +40,9 @@ function SetButtons(){
 		}
 	}
 	//initialise bricks
-	for(var i = 0; i < 18; i++){
+	for(var i = 0; i < rows; i++){
 		bricks[i] = new Array(); 
-		for(var j = 0; j < 27; j++){
+		for(var j = 0; j < cols; j++){
 			bricks[i][j] = new Brick();			
 		}
 	}
@@ -39,26 +53,25 @@ function ClickAction(obj){
 	var id = obj.id;
 	if(startMark){
 		ClickFirst(id);
-		DisplayNumber(Math.floor(id/27),id%27);		
+		DisplayNumber(Math.floor(id/cols), id%cols);		
 		startMark = false;
 	}
 	else{
-		if(!GameOver(Math.floor(id/27),id%27)){
-			DisplayNumber(Math.floor(id/27),id%27);
-			//document.write(mines);
+		if(!GameOver(Math.floor(id/cols), id%cols)){
+			DisplayNumber(Math.floor(id/cols), id%cols);
 		}
 	}	
 }
 
 //first time to click and set all mines
 function ClickFirst(id){
-	var speicialBricks = NineId(id);	
-	//document.write(speicialBricks);			
+	var speicialBricks = NineId(id);				
 	//put mines randomly
 	for(var i = 0; i < mineNum; i++){
-		var randomMine = Math.floor(Math.random() * 27 * 18);
-		if(mines.indexOf(randomMine)<0 && speicialBricks.indexOf(randomMine)<0){
-			if(randomMine > 27 && randomMine < 458 && randomMine % 27 != 0 && randomMine % 27 != 26){
+		var randomMine = Math.floor(Math.random() * cols * rows);
+		if(speicialBricks.indexOf(randomMine)<0 && mines.indexOf(randomMine)<0){
+			if(randomMine > cols && randomMine < ((rows-1)*cols-1) 
+				&& randomMine%cols!=0 && (randomMine%cols!=(cols-1))){
 				mines[i] = randomMine;	
 				//document.getElementById(randomMine).style.backgroundColor = "#f00";
 			}else{
@@ -68,45 +81,77 @@ function ClickFirst(id){
 			i--;
 		}
 	}
-	//document.writeln(mines);
 	// mark mines
-	for(var i=0;i<bricks.length;i++){
-		for(var j=0;j<bricks[i].length;j++){
-			if(mines.indexOf(i*27+j)>=0){
+	for(var i = 0; i < bricks.length; i++){
+		for(var j = 0; j < bricks[i].length; j++){
+			if(mines.indexOf(i*cols+j)>=0){
 				bricks[i][j].isMine = true;
 			}			
 		}
 	}
 	//cont the number of mines around the current brick
-	for(var i = 1; i<bricks.length-1;i++){
-		for(var j=1; j<bricks[i].length-1;j++){
-			if(bricks[i][j].isMine==false){
-				for(var m=0;m<9;m++){
+	for(var i = 1; i < bricks.length - 1; i++){
+		for(var j = 1; j < bricks[i].length - 1; j++){
+			if(bricks[i][j].isMine == false){
+				for(var m = 0; m < 9; m++){
 					var x = NineBricks(i,j)[m][0];
 					var y = NineBricks(i,j)[m][1];
-					if(m!=4 && bricks[x][y].isMine == true){
-						bricks[i][j].totalNum+=1;
-						bricks[i][j].leftNum+=1;
+					if(m != 4 && bricks[x][y].isMine == true){
+						bricks[i][j].totalNum += 1;
+						bricks[i][j].leftNum += 1;
 					}
 				}
 			}			
-			//document.getElementById(i*27+j).innerHTML = bricks[i][j].totalNum;
 		}
 	}
 
 }
 
+
+function DisplayNumber(i,j){
+
+	if(i>0 && i<rows-1 && j>0 && j<cols-1 && bricks[i][j].isClicked==false){
+
+		document.getElementById(i*cols+j).style.backgroundColor = "#eee";
+		document.getElementById(i*cols+j).disabled = true;
+		bricks[i][j].isClicked = true;
+
+		if(bricks[i][j].totalNum != 0){
+			document.getElementById(i*cols+j).style.color = NumberColor(bricks[i][j].totalNum);
+			document.getElementById(i*cols+j).innerHTML = bricks[i][j].totalNum;
+		}
+		else{
+			for(var m = 0; m<9; m++){
+				var x = NineBricks(i,j)[m][0];
+				var y = NineBricks(i,j)[m][1]; 	
+				DisplayNumber(x,y);			
+			}	
+		}
+	}
+}
+
+function GameOver(i,j){
+	if(bricks[i][j].isMine){
+		for(var m=0;m<mines.length;m++){
+			//document.writeln(mines[m]);
+			document.getElementById(mines[m]).style.backgroundColor = "#f00";
+		}
+		return true;
+	}
+	return false;
+}
+
 function NineId(id){
 	return [
-			id-1-27,
-			id-27, 
-			id-27+1,
+			id-1-cols,
+			id-cols, 
+			id-cols+1,
 			id-1,
-			id,
+			id-0,
 			id-(-1),
-			id-1-(-27),
-			id-(-27),
-			id-(-27-1)
+			id-1-(-cols),
+			id-(-cols),
+			id-(-cols-1)
 		];
 }
 
@@ -123,69 +168,6 @@ function NineBricks(i,j){
 		[i+1,j+1]
 	];
 }
-
-// class of single brick
-function Brick(){
-
-	this.isMine = false; 		//bool
-	this.totalNum = 0; 	//total number of mines
-	this.leftNum = 0; 	//the number of unknown mines
-	this.isClicked = false;
-	this.setMineState = function(state){
-		this.isMine = state;
-		return this.isMine;
-	}
-	this.setTotalNum = function(num){
-		this.totalNum = num;
-		this.leftNum = num;
-	}
-	this.setLeftNum =  function(){
-		this.leftNum--;
-	}
-}
-
-function DisplayNumber(i,j){
-
-	if(bricks[i][j].totalNum != 0){
-		document.getElementById(i*27+j).style.backgroundColor = "#eee";
-		document.getElementById(i*27+j).style.color = NumberColor(bricks[i][j].totalNum);
-		document.getElementById(i*27+j).innerHTML = bricks[i][j].totalNum;
-		document.getElementById(i*27+j).disabled = true;
-		bricks[i][j].isClicked = true;
-	}
-	else{
-		for(var m = 0; m<9; m++){
-			var x = NineBricks(i,j)[m][0];
-			var y = NineBricks(i,j)[m][1]; 
-			if(x>0&&x<17&&y>0&&y<26&&bricks[x][y].isClicked==false){
-				if(bricks[x][y].totalNum == 0){
-					document.getElementById(x*27+y).style.backgroundColor = "#eee";
-					document.getElementById(x*27+y).disabled = true;
-					bricks[x][y].isClicked = true;
-					DisplayNumber(x,y);
-				}
-				else{
-				    document.getElementById(x*27+y).style.backgroundColor = "#eee";
-				    document.getElementById(x*27+y).style.color = NumberColor(bricks[x][y].totalNum);
-					document.getElementById(x*27+y).innerHTML = bricks[x][y].totalNum;
-					document.getElementById(x*27+y).disabled = true;
-					bricks[x][y].isClicked = true;
-				}
-			}	
-		}
-	}	
-}
-
-function GameOver(i,j){
-	if(bricks[i][j].isMine){
-		for(var m=0;m<mines.length;m++){
-			document.getElementById(mines[m]).style.backgroundColor = "#f00";
-		}
-		return true;
-	}
-	return false;
-}
-
 function NumberColor(num){
 	var cr = "#000";
 	switch(num){
